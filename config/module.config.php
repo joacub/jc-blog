012fbadfcb54f42582f88e9726e0ae1bbb30dd9e
Johan <joacub@gmail.com>
2013-09-06 10:05:19 +0200

    cambios
    Signed-off-by: Johan <joacub@gmail.com>

--- config/module.config.php
+++ config/module.config.php
@@ -4,23 +4,23 @@ return array(
 	'JcBlog' => array(
 		'posts-per-page' => 10,
 		'date-format' => '%e %b %Y',
-	    'entity_class' => 'JcBlog\Entity\ByDefault\Post',
-	    'enable_default_entities' => true,
+		'entity_class' => 'JcBlog\Entity\ByDefault\Post',
+		'enable_default_entities' => true
 	),
-    'doctrine' => array(
-        'driver' => array(
-            'JcBlog_entities' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
-                'paths' => __DIR__ . '/xml/jcblog'
-            ),
-    
-            'orm_default' => array(
-                'drivers' => array(
-                    'JcBlog\Entity\Super'  => 'JcBlog_entities'
-                )
-            )
-        )
-    ),
+	'doctrine' => array(
+		'driver' => array(
+			'JcBlog_entities' => array(
+				'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
+				'paths' => __DIR__ . '/xml/jcblog'
+			),
+			
+			'orm_default' => array(
+				'drivers' => array(
+					'JcBlog\Entity\Super' => 'JcBlog_entities'
+				)
+			)
+		)
+	),
 	'router' => array(
 		'routes' => array(
 			'zfcadmin' => array(
@@ -30,9 +30,10 @@ return array(
 						'options' => array(
 							'route' => '/' . __NAMESPACE__,
 							'defaults' => array(
-								'controller' => __NAMESPACE__ . '\Controller\Admin\Index',
-								'action' => 'index',
-							),
+								'controller' => __NAMESPACE__ .
+										 '\Controller\Admin\Index',
+										'action' => 'index'
+							)
 						),
 						'may_terminate' => true,
 						'child_routes' => array(
@@ -41,18 +42,19 @@ return array(
 								'options' => array(
 									'route' => '/post[/:action[/:id]]',
 									'defaults' => array(
-										'controller' => __NAMESPACE__ . '\Controller\Admin\Post',
+										'controller' => __NAMESPACE__ .
+										 '\Controller\Admin\Post',
 										'action' => 'index',
-										'id' => 0,
+										'id' => 0
 									),
 									'constraints' => array(
-										'id' => '[0-9]+',
-									),
-								),
-							),
-						),
-					),
-				),
+										'id' => '[0-9]+'
+									)
+								)
+							)
+						)
+					)
+				)
 			),
 			'JcBlog' => array(
 				'type' => 'Segment',
@@ -105,9 +107,11 @@ return array(
 	'controllers' => array(
 		'invokables' => array(
 			'JcBlog\Controller\Post' => 'JcBlog\Controller\PostController',
-			__NAMESPACE__ . '\Controller\Admin\Index' => __NAMESPACE__ . '\Controller\Admin_IndexController',
-			__NAMESPACE__ . '\Controller\Admin\Post' => __NAMESPACE__ . '\Controller\Admin_PostController',
-		),
+			__NAMESPACE__ . '\Controller\Admin\Index' => __NAMESPACE__ .
+			 '\Controller\Admin_IndexController',
+			__NAMESPACE__ . '\Controller\Admin\Post' => __NAMESPACE__ .
+			 '\Controller\Admin_PostController'
+		)
 	),
 	'navigation' => array(
 		'admin' => array(
@@ -118,41 +122,47 @@ return array(
 					'post-list' => array(
 						'label' => 'Listado de páginas',
 						'route' => 'zfcadmin/' . __NAMESPACE__ . '/posts',
-						'params' => array('action' => 'list'),
+						'params' => array(
+							'action' => 'list'
+						),
 						'pages' => array(
 							'articles-create' => array(
 								'label' => 'Nueva página',
 								'route' => 'zfcadmin/' . __NAMESPACE__ . '/posts',
-								'params' => array('action' => 'create'),
-							),
+								'params' => array(
+									'action' => 'create'
+								)
+							)
 						)
 					),
 					'post-create' => array(
 						'label' => 'Nueva página',
 						'route' => 'zfcadmin/' . __NAMESPACE__ . '/posts',
-						'params' => array('action' => 'create'),
-					),
-				),
-			),
-		),
+						'params' => array(
+							'action' => 'create'
+						)
+					)
+				)
+			)
+		)
 	),
 	'view_manager' => array(
 		'template_path_stack' => array(
 			'JcBlog' => __DIR__ . '/../view'
 		),
 		'template_map' => array(
-			'jc-navigation/toolbar/jc-blog-pages'
-			=> __DIR__ . '/../view/jc-navigation/toolbar/jc-blog-pages.phtml',
-		),
+			'jc-navigation/toolbar/jc-blog-pages' => __DIR__ .
+			 '/../view/jc-navigation/toolbar/jc-blog-pages.phtml'
+		)
 	),
 	
 	'service_manager' => array(
 		'factories' => array(
-			'JcBlog\Collector\MenuCollector'  => 'JcBlog\Service\MenuCollectorServiceFactory',
+			'JcBlog\Collector\MenuCollector' => 'JcBlog\Service\MenuCollectorServiceFactory'
 		),
 		'aliases' => array(
-			'jcblog_doctrine_em' => 'Doctrine\ORM\EntityManager',
-		),
+			'jcblog_doctrine_em' => 'Doctrine\ORM\EntityManager'
+		)
 	),
 	
 	'JcNavigation' => array(
@@ -166,5 +176,15 @@ return array(
 				'jc_blog_pages_collector' => 'jc-navigation/toolbar/jc-blog-pages'
 			)
 		)
+	),
+	'ZfjPageBanner' => array(
+		'routers' => array(
+			'JcBlog-post' => array(
+				'entity' => 'JcBlog\Entity\ByDefault',
+				'identifier-param' => 'slug',
+				'identifier-db' => 'slug'
+			)
+		)
 	)
+	
 );
